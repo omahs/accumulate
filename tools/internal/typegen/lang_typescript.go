1563af93da70298cb2be098e388dd47756be6bd6
Ethan Reesor <ethan.reesor@gmail.com>
2022-11-26 00:59:21 -0600

    saving work

--- tools/internal/typegen/lang_typescript.go
+++ tools/internal/typegen/lang_typescript.go
@@ -15,9 +15,9 @@ func (f FieldType) TypescriptType() string {
 	case TypeCodeRawJson:
 		return "unknown"
 	case TypeCodeUrl:
-		return "url.URL"
+		return "URL"
 	case TypeCodeTxid:
-		return "url.TxID"
+		return "TxID"
 	case TypeCodeBigInt:
 		return "BN"
 	case TypeCodeUint:
@@ -30,6 +30,37 @@ func (f FieldType) TypescriptType() string {
 		return "number"
 	case TypeCodeTime:
 		return "Date"
+	case TypeCodeBool:
+		return "boolean"
+	default:
+		return f.Code.String()
+	}
+}
+
+func (f FieldType) TypescriptInputType() string {
+	switch f.Code {
+	case TypeCodeUnknown:
+		return f.Name
+	case TypeCodeBytes:
+		return "Uint8Array | string"
+	case TypeCodeRawJson:
+		return "unknown"
+	case TypeCodeUrl:
+		return "URL | string"
+	case TypeCodeTxid:
+		return "TxID | string"
+	case TypeCodeBigInt:
+		return "BN | string"
+	case TypeCodeUint:
+		return "number"
+	case TypeCodeInt:
+		return "number"
+	case TypeCodeHash:
+		return "Uint8Array | string"
+	case TypeCodeDuration:
+		return "number"
+	case TypeCodeTime:
+		return "Date | string"
 	case TypeCodeBool:
 		return "boolean"
 	default:
